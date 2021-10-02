aa9507069ee10d409f498a3bb6173f814ddf74b3
hexdump0815 <hexdump0815@googlemail.com>
2021-10-02 19:19:18 +0200

    silence logspam from unhandled signals and unimplemented syscalls
    see those two mainline commits for reference:
    https://github.com/torvalds/linux/commit/5ee39a71fd89ab7240c5339d04161c44a8e03269
    https://github.com/torvalds/linux/commit/1962682d2b2fbe6cfa995a85c53c069fadda473e

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -48,7 +48,7 @@ static const char *handler[]= {
 	"Error"
 };
 
-int show_unhandled_signals = 1;
+int show_unhandled_signals = 0;
 
 /*
  * Dump out the contents of some memory nicely...
@@ -396,14 +396,6 @@ asmlinkage long do_ni_syscall(struct pt_regs *regs)
 			return ret;
 	}
 #endif
-
-	if (show_unhandled_signals && printk_ratelimit()) {
-		pr_info("%s[%d]: syscall %d\n", current->comm,
-			task_pid_nr(current), (int)regs->syscallno);
-		dump_instr("", regs);
-		if (user_mode(regs))
-			__show_regs(regs);
-	}
 
 	return sys_ni_syscall();
 }
